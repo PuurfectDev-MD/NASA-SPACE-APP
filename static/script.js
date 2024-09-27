3fc2e6aee14a76ca9f12d4874da11a6b6698aa4f
PuurfectDev-MD <new.manishd@gmail.com>
2024-09-27 21:17:14 +0400

    added slideshow and fixed all issues with tying effect

--- static/script.js
+++ static/script.js
@@ -1,3 +1,29 @@
+// Slideshow functionality
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  captionText.innerHTML = slides[slideIndex-1].alt;
+}
+
+// Typing effect functionality
 const words = ["Explore", "Learn", "Understand"];  // The words to type and delete
 const typingSpeed = 200;  // Speed of typing (in milliseconds)
 const deletingSpeed = 150;  // Speed of deleting (in milliseconds)
