7c4a0659c32bc66c47b36a1e6c3d4d10f39d3bea
PuurfectDev-MD <new.manishd@gmail.com>
2024-10-04 19:29:11 +0400

    added exoplanets - partially complete

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\manis\NASA SPACE APPS\.venv
+set VIRTUAL_ENV=c:\Users\manis\Downloads\NASA-space-fork-main\NASA-space-fork-main\NASA Fork\NASA Fork\NASA-SPACE-APP\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app= Flask(__name__)
 def home():
     return render_template("homepage.html")
 
+@app.route("/exo")
+def exo():
+    return render_template("exoplanet.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
